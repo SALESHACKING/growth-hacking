223f0fbdf90e6a17fd04693dc06f404c825df9f0
Stephen <85037932+SALESHACKING@users.noreply.github.com>
2022-02-06 17:40:54 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,13 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <p>Let&apos;s GROW est la r&eacute;f&eacute;rence
+           <span>&nbsp;est la r&eacute;f&eacute;rence des ressources &amp; bonnes pratiques dans le domaine du Growth Hacking et de l&apos;entrepreneuriat au sens large.&nbsp;</span>
         </p>
+        <p>Nous aidons gratuitement les entrepreneurs, growth hackers et commerciaux &agrave; obtenir tout le n&eacute;cessaire pour booster leur croissance.<br style="box-sizing: border-box;">Qu&apos;il s&apos;agisse de novices ou de profils chevronn&eacute;s, nous alimentons chaque jour une base de :<br style="box-sizing: border-box;">✅ Des centaines de&nbsp;<a href="https://sales-hacking.com/growth-hacks" rel="noopener noreferrer" target="_blank">growth hacks</a> enrichis CHAQUE JOUR<br style="box-sizing: border-box;">✅ Pr&egrave;s de 100&nbsp;<a href="https://www.sales-hacking.com/growth-ressources">ressources pour entrepreneurs</a> t&eacute;l&eacute;chargeables,<br style="box-sizing: border-box;">✅ 100 articles de blog autour du <a href="https://www.sales-hacking.com/blog-categories/growth-hacking">growth hacking</a>, de <a href="https://www.sales-hacking.com/blog-categories/seo-content-marketing">content marketing</a>, de techniques pour les Sales, du&nbsp;<a href="https://www.sales-hacking.com/blog-categories/web-scraping">Web scraping</a>, de Lead Generation et de l&apos;entrepreneuriat en g&eacute;n&eacute;ral,<br style="box-sizing: border-box;">✅ Des conseils de lecture avec <a href="https://www.sales-hacking.com/livres">le meilleur des livres business</a><br style="box-sizing: border-box;">✅ Des applications SaaS</p>
+        
+
+        <p><br>En bref, on essaie de faire tout pour que vous r&eacute;ussissiez ❤️</p>
       </main>
 
       <Footer />
