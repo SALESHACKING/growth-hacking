e4c07f86807c0aadfbdf58547c04e7ac3a0312ac
Stephen <85037932+SALESHACKING@users.noreply.github.com>
2022-02-07 07:24:53 -0500

    Update index.js
    Div class infobox added

--- pages/index.js
+++ pages/index.js
@@ -11,17 +11,19 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-    
-        <p>Lets GROW est la référence des ressources : bonnes pratiques dans le domaine du Growth Hacking et de l&apos;entrepreneuriat au sens large.&nbsp;</p>
-        <p>Nous aidons gratuitement les entrepreneurs, growth hackers et commerciaux &agrave; obtenir tout le nécessaire pour booster leur croissance.</p>
-        <p><br style="box-sizing: border-box;">Qu&apos;il s&apos;agisse de novices ou de profils chevronnés, nous alimentons chaque jour une base de :</br></p>
-        <p><br style="box-sizing: border-box;">✅ Des centaines de&nbsp;<a href="https://sales-hacking.com/growth-hacks" rel="noopener noreferrer" target="_blank">growth hacks</a> enrichis CHAQUE JOUR</br></p>
-        <p><br style="box-sizing: border-box;">✅ Pr&egrave;s de 100&nbsp;<a href="https://www.sales-hacking.com/growth-ressources">ressources pour entrepreneurs</a> téléchargeables</br>,</p>
-        <p><br style="box-sizing: border-box;">✅ 100 articles de blog autour du <a href="https://www.sales-hacking.com/blog-categories/growth-hacking">growth hacking</a>, de <a href="https://www.sales-hacking.com/blog-categories/seo-content-marketing">content marketing</a>, de techniques pour les Sales, du&nbsp;<a href="https://www.sales-hacking.com/blog-categories/web-scraping">Web scraping</a>, de Lead Generation et de l&apos;entrepreneuriat en général,</br></p>
-        <p><br style="box-sizing: border-box;">✅ Des conseils de lecture avec <a href="https://www.sales-hacking.com/livres">le meilleur des livres business</a></br></p>
-        <p><br style="box-sizing: border-box;">✅ Des applications SaaS</br></p>
-      </main>
+        <div class="infobox">
+          <Header title="Welcome to my app!" />
+
+          <p>Lets GROW est la référence des ressources : bonnes pratiques dans le domaine du Growth Hacking et de l&apos;entrepreneuriat au sens large.&nbsp;</p>
+          <p>Nous aidons gratuitement les entrepreneurs, growth hackers et commerciaux &agrave; obtenir tout le nécessaire pour booster leur croissance.</p>
+          <p><br style="box-sizing: border-box;">Qu&apos;il s&apos;agisse de novices ou de profils chevronnés, nous alimentons chaque jour une base de :</br></p>
+          <p><br style="box-sizing: border-box;">✅ Des centaines de&nbsp;<a href="https://sales-hacking.com/growth-hacks" rel="noopener noreferrer" target="_blank">growth hacks</a> enrichis CHAQUE JOUR</br></p>
+          <p><br style="box-sizing: border-box;">✅ Pr&egrave;s de 100&nbsp;<a href="https://www.sales-hacking.com/growth-ressources">ressources pour entrepreneurs</a> téléchargeables</br>,</p>
+          <p><br style="box-sizing: border-box;">✅ 100 articles de blog autour du <a href="https://www.sales-hacking.com/blog-categories/growth-hacking">growth hacking</a>, de <a href="https://www.sales-hacking.com/blog-categories/seo-content-marketing">content marketing</a>, de techniques pour les Sales, du&nbsp;<a href="https://www.sales-hacking.com/blog-categories/web-scraping">Web scraping</a>, de Lead Generation et de l&apos;entrepreneuriat en général,</br></p>
+          <p><br style="box-sizing: border-box;">✅ Des conseils de lecture avec <a href="https://www.sales-hacking.com/livres">le meilleur des livres business</a></br></p>
+          <p><br style="box-sizing: border-box;">✅ Des applications SaaS</br></p>
+        </div>  
+    </main>
 
       <Footer />
     </div>
