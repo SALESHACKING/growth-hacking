ebc52f8b070a585b62f0515de9477b1f2308ba36
Stephen <85037932+SALESHACKING@users.noreply.github.com>
2022-02-07 07:32:04 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Home() {
       </Head>
 
       <main>
-        <div class="infobox">
           <Header title="Welcome to my app!" />
 
           <p>Lets GROW est la référence des ressources : bonnes pratiques dans le domaine du Growth Hacking et de l&apos;entrepreneuriat au sens large.&nbsp;</p>
@@ -22,7 +21,6 @@ export default function Home() {
           <p><br style="box-sizing: border-box;">✅ 100 articles de blog autour du <a href="https://www.sales-hacking.com/blog-categories/growth-hacking">growth hacking</a>, de <a href="https://www.sales-hacking.com/blog-categories/seo-content-marketing">content marketing</a>, de techniques pour les Sales, du&nbsp;<a href="https://www.sales-hacking.com/blog-categories/web-scraping">Web scraping</a>, de Lead Generation et de l&apos;entrepreneuriat en général,</br></p>
           <p><br style="box-sizing: border-box;">✅ Des conseils de lecture avec <a href="https://www.sales-hacking.com/livres">le meilleur des livres business</a></br></p>
           <p><br style="box-sizing: border-box;">✅ Des applications SaaS</br></p>
-        </div>  
     </main>
 
       <Footer />
